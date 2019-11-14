dd1cf261b0b99e9be7c605586975219aa72c4319
Voodooley <voodooley83@gmail.com>
2019-11-14 22:19:29 +0300

    Добавил еще одно решение на определение вис года

--- is_year_leap.py
+++ is_year_leap.py
@@ -5,6 +5,14 @@ def is_year_leap(year):
     else:
         return False
 
+
+def is_year_leap2(ye):
+    return not bool(ye % 4)
+
+
 year = int(input('Введите год для проверки: '))
 
 print(is_year_leap(year))
+
+year2 = int(input('Введите год2 для проверки: '))
+print(is_year_leap2(year2))
